662092f7850a7d620a7365c9069298f8a348743c
kimmmm <khjjung1124@gmail.com>
2022-01-07 21:09:08 +0900

    reupload

--- src/main/java/com/example/Chapter06Application.java
+++ src/main/java/com/example/Chapter06Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Chapter06Application {
 
     public static void main(String[] args) {
-
+        //check
         SpringApplication.run(Chapter06Application.class, args);
     }
 
